ddf142d2738337a7a1ed1b88832c352bf474dd11
ESTEVAO <estevao.serpa1234@gmail.com>
2021-12-21 21:35:13 -0300

    add modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import logoImg from '../../assets/logo.svg'
+
 import { Container, Content } from './styles'
 
+interface headerProps{
+    onOpenNewTransactionModal:() => void;
+}
+export function Header({onOpenNewTransactionModal}:headerProps) {
 
-export function Header() {
+ 
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>
