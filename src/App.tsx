94a1872fc28387dd803d7eb4cc396426c8e4bb44
ESTEVAO <estevao.serpa1234@gmail.com>
2021-12-22 09:20:53 -0300

    add componente transactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
-import Modal from 'react-modal';
+
 import { useState } from 'react'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal';
 
-Modal.setAppElement('#root')
+
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
-  function handleOpenNewTransactionModal(){
-      setIsNewTransactionModalOpen(true)
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
   }
-  function handleCloseNewTransactionModal(){
-      setIsNewTransactionModalOpen(false)
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
   }
   return (
     <>
       <GlobalStyle />
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>batata</h2>
-      </Modal>
+      />
+
     </>
   );
 }
